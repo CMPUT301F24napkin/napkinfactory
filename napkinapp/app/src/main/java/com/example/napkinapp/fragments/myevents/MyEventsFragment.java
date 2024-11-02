3d48cadd1b0e0c7fa6ef8c2560ad373642555770
Stefan Antoszko <santoszk@ualberta.ca>
2024-11-01 19:25:04 -0600

    Created a MyEvents page.

--- napkinapp/app/src/main/java/com/example/napkinapp/fragments/myevents/MyEventsFragment.java
+++ napkinapp/app/src/main/java/com/example/napkinapp/fragments/myevents/MyEventsFragment.java
@@ -1,4 +1,9 @@
-package com.example.napkinapp.fragments.listevents;
+/**
+ * This file is literally the same as ListEventsFragment.java however the parameters to the constructor
+ * of the EventArrayAdapter are different to allow for different button look and functionality.
+ */
+
+package com.example.napkinapp.fragments.myevents;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -12,19 +17,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.napkinapp.TitleUpdateListener;
-import com.example.napkinapp.models.Event;
 import com.example.napkinapp.R;
+import com.example.napkinapp.TitleUpdateListener;
+import com.example.napkinapp.fragments.listevents.EventArrayAdapter;
+import com.example.napkinapp.models.Event;
 
 import java.util.ArrayList;
 import java.util.Date;
 
-public class ListEventsFragment extends Fragment {
+public class MyEventsFragment extends Fragment {
+    private ListView eventslist;
+    private ArrayList<Event> events;
+    private EventArrayAdapter eventArrayAdapter;
     private TitleUpdateListener titleUpdateListener;
-    private Context mContext;
 
-    public ListEventsFragment(){
-        // Required null constructor
+    public MyEventsFragment() {
+
     }
 
     @Override
@@ -36,37 +44,32 @@ public class ListEventsFragment extends Fragment {
         }else{
             throw new RuntimeException(context + " needs to implement TitleUpdateListener");
         }
-
-        mContext = context;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.event_list, container, false);
-        ListView eventslist;
-        ArrayList<Event> events;
-        EventArrayAdapter eventArrayAdapter;
 
         eventslist = view.findViewById(R.id.events_list_view);
         events = new ArrayList<>();
 
         //Update title
-        titleUpdateListener.updateTitle("Event List");
+        titleUpdateListener.updateTitle("My Events");
 
         // Add sample events
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 7; i++) {
             events.add(new Event(String.valueOf(i), "Event " + i, new Date()));
         }
 
         // Attach EventArrayAdapter to ListView
-        eventArrayAdapter = new EventArrayAdapter(mContext, events, "Add to Watchlist", R.drawable.add, v -> {
+        eventArrayAdapter = new EventArrayAdapter(getContext(), events, "View", 0, v -> {
             Event event = (Event)v.getTag();
             Log.i("Button", String.format("Clicked on event %s\n", event.getName()));
         });
         eventslist.setAdapter(eventArrayAdapter);
 
-        Log.d("ListEventsFragment", "Event list loaded with " + events.size() + " items.");
+        Log.d("MyEventsFragment", "Event list loaded with " + events.size() + " items.");
         return view;
     }
 }
