9d61c6910cf45957516f1ce3555a59a937fc435b
jjedrych <jedrych@ualberta.ca>
2024-11-27 15:22:24 -0700

    first iteration of ImageUtils

--- napkinapp/app/src/main/java/com/example/napkinapp/utils/ImageUtils.java
+++ napkinapp/app/src/main/java/com/example/napkinapp/utils/ImageUtils.java
@@ -7,37 +7,36 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-public class ImageUtils {
-    private static FirebaseStorage storage = FirebaseStorage.getInstance();
+import java.util.Objects;
 
-    // Constants for folders on Firebase
+public class ImageUtils {
+    private static FirebaseStorage FirebaseInt = FirebaseStorage.getInstance();
+
+    // Constants for folders on Firebase (MIGHT DELETE LATER)
     public static final String PROFILE = "profiles";
     public static final String EVENT = "events";
 
     /**
-     * Returns the relative path of a image on the firebase cloud
-     * @param uri image on firebase
-     * @return path to the image on firebase
-     */
-    public static String getFilePathFromUri(Uri uri) {
-        // Get the full path in Firebase Storage from the URI
-        StorageReference ref = storage.getReferenceFromUrl(uri.toString());
-        return ref.getPath();  // This returns the file path, e.g., "/profiles/12345.jpg"
-    }
-
-    /**
-     * uploads image to firebase cloud
+     * Uploads images related to specific functionality of the app to firebase cloud
+     *
      * @param image local image to upload to firebase
-     * @param destinationFolder Folder where images should be uploaded based on types
-     *                          (WE HAVE CONSTANTS IN THE UTIL CLASS FOR THIS!!!)
-     * @return
+     * @return Task<Uri> that once resolved will return the Uri of the upload onSuccess
      */
-    // Upload image and return the Firebase path (relative path) to the image
-    public static Task<Uri> uploadImage(Uri image, String destinationFolder) {
+    public static Task<Uri> uploadImage(Uri image) {
+        // grab file extension
+        String path = image.getPath();
+        String ext;
+        if (path != null && path.contains(".")) {
+            ext = path.substring(path.lastIndexOf(".") + 1);
+        }
+        else {
+            return null;
+        }
+
         // Create a reference to the Firebase Storage location
-        StorageReference storageRef = storage.getReference().child(destinationFolder);
-        String fileName = System.currentTimeMillis() + ".jpg";  // Generate unique file name
-        StorageReference fileRef = storageRef.child(fileName);
+        StorageReference folderRef = FirebaseInt.getReference().child("IMAGES");
+        String fileName = System.currentTimeMillis() + ext;  // Generate unique file name
+        StorageReference fileRef = folderRef.child(fileName);
 
         // Start the upload task
         UploadTask uploadTask = fileRef.putFile(image);
@@ -45,19 +44,39 @@ public class ImageUtils {
         // Return the Task that will resolve with the download URL once successful
         return uploadTask.continueWithTask(task -> {
             if (!task.isSuccessful()) {
-                throw task.getException(); // Throw the exception to be caught in the onFailureListener
+                throw Objects.requireNonNull(task.getException()); // Throw the exception to be caught in the onFailureListener
             }
             // Once upload is successful, retrieve the download URL (Task<Uri>)
             return fileRef.getDownloadUrl();
         });
     }
 
-    // Get image Uri from Firebase Storage based on the image path
-    public static Task<Uri> getImageUri(String imagePath) {
-        // Create reference to the image file in Firebase Storage
-        StorageReference storageRef = storage.getReference().child(imagePath);
+    /**
+     * Deletes an image from Firebase Storage.
+     *
+     * @param firebaseImage Uri of the image on Firebase to be deleted.
+     * @return Task<Void> that resolves on successful deletion.
+     */
+    public static Task<Void> deleteImage(Uri firebaseImage) {
+        StorageReference imageRef = FirebaseInt.getReferenceFromUrl(firebaseImage.toString());
 
-        // Return the Task<Uri> directly
-        return storageRef.getDownloadUrl();
+        return imageRef.delete();
+    }
+
+    /**
+     * Replaces a firebase image with a new image from local storage
+     *
+     * @param firebaseImage image on firebase that is going to be replaced
+     * @param newImage new image on local storage
+     * @return Task<Uri> that once resolved will return the Uri of the new image onSuccess
+     */
+    public static Task<Uri> updateImage(Uri firebaseImage, Uri newImage) {
+        return deleteImage(firebaseImage).continueWithTask(deleteTask -> {
+            if (!deleteTask.isSuccessful()) {
+                throw Objects.requireNonNull(deleteTask.getException()); // Throw if delete fails
+            }
+            // upload new image
+            return uploadImage(newImage);
+        });
     }
 }
