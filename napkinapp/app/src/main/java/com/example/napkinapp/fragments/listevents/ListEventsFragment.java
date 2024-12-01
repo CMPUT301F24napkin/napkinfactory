74b07bee6f2f63b180303c43635f4c58aeb0c031
davidurschel <durschel@ualberta.ca>
2024-11-30 20:44:05 -0700

    Purple no more

--- napkinapp/app/src/main/java/com/example/napkinapp/fragments/listevents/ListEventsFragment.java
+++ napkinapp/app/src/main/java/com/example/napkinapp/fragments/listevents/ListEventsFragment.java
@@ -9,13 +9,12 @@ package com.example.napkinapp.fragments.listevents;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
-
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,15 +24,14 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.napkinapp.R;
 import com.example.napkinapp.TitleUpdateListener;
 import com.example.napkinapp.fragments.viewevents.ViewEventFragment;
 import com.example.napkinapp.models.Event;
-import com.example.napkinapp.R;
 import com.example.napkinapp.models.User;
 import com.example.napkinapp.utils.DB_Client;
 import com.example.napkinapp.utils.Location_Utils;
@@ -153,10 +151,12 @@ public class ListEventsFragment extends Fragment {
             // It is waitlisted
             btn.setText(R.string.remove_from_waitlist);
             btn.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.remove, 0, 0, 0);
+            btn.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(mContext, R.color.colorRemoveDark)));
             btn.setSelected(true);
         } else {
             // It is not
             btn.setText(R.string.add_to_waitlist);
+            btn.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(mContext, R.color.neutralGray)));
             btn.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.add, 0, 0, 0);
             btn.setSelected(false);
         }
