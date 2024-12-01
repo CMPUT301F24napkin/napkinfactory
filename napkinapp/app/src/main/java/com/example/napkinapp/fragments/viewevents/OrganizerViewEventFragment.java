8d379414b06f73b013a285f27ffd1c2ba9bf4720
Stefan Antoszko <santoszk@ualberta.ca>
2024-11-30 19:55:08 -0700

    disabled interactivity on requireGeolocation switch and turned off the map if requireGeolocation is false.

--- napkinapp/app/src/main/java/com/example/napkinapp/fragments/viewevents/OrganizerViewEventFragment.java
+++ napkinapp/app/src/main/java/com/example/napkinapp/fragments/viewevents/OrganizerViewEventFragment.java
@@ -490,12 +490,29 @@ public class OrganizerViewEventFragment extends Fragment {
             }
         });
 
-        // populate map
-        for(ArrayList<Double> location : event.getEntrantLocations().values()) {
-            if(location.size() < 2) {
-                Log.e("Map", "Event Entrant Locations coordinate has less than 2 elements! expecting 2, one for longitude and latitude.");
+        if(event.isRequireGeolocation()) {
+            map.setTileSource(TileSourceFactory.MAPNIK);
+            requestMapPermissionLauncher.launch(permissionsToRequest);
+            map.setMultiTouchControls(true);
+            IMapController mapController = map.getController();
+            mapController.setZoom(15.0);
+            GeoPoint startPoint = new GeoPoint(53.527309714453466, -113.52931950296305);
+            mapController.setCenter(startPoint);
+            map.setOnTouchListener((v, event) -> {
+                // Request parent to not intercept touch events when MapView is touched
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;  // Let the MapView handle the touch event
+            });
+
+            // populate map
+            for(ArrayList<Double> location : event.getEntrantLocations().values()) {
+                if(location.size() < 2) {
+                    Log.e("Map", "Event Entrant Locations coordinate has less than 2 elements! expecting 2, one for longitude and latitude.");
+                }
+                addMarker(map, location.get(0), location.get(1));
             }
-            addMarker(map, location.get(0), location.get(1));
+        } else {
+            map.setVisibility(View.GONE);
         }
 
         return view;
