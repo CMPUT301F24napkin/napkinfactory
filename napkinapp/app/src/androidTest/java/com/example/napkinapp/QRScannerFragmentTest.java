ded814eef01467cb6e55e56ed629de165bde97fc
jjedrych <jedrych@ualberta.ca>
2024-12-02 13:50:28 -0700

    Abs do this now

--- napkinapp/app/src/androidTest/java/com/example/napkinapp/QRScannerFragmentTest.java
+++ napkinapp/app/src/androidTest/java/com/example/napkinapp/QRScannerFragmentTest.java
@@ -1,18 +1,35 @@
 package com.example.napkinapp;
 
+import androidx.fragment.app.Fragment;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
 import com.example.napkinapp.fragments.qrscanner.QRScannerFragment;
+import com.example.napkinapp.fragments.viewevents.ViewEventFragment;
 import com.example.napkinapp.utils.AbstractFragmentTest;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.BarcodeView;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -24,11 +41,19 @@ public class QRScannerFragmentTest extends AbstractFragmentTest<QRScannerFragmen
     }
 
     @Test
-    public void testQRScannerFragment_isDisplayed() {
+    public void testQRScannerIsDisplayed(){
         // Check if the QR scanner view is displayed
-        onView(withId(R.layout.qr_scanner)).check(matches(isDisplayed()));
-        onView(withId(R.id.header_title)).check(matches(withText("QR Scanner")));
+        onView(withId(R.id.barcode_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.qr_overlay)).check(matches(isDisplayed()));
+        onView(withId(R.id.scan_instructions)).check(matches(withText("Align QR code within the frame")));
     }
+
+    @Test
+    public void testQRScannerOpensViewEvent(){
+        onView(withId(R.id.barcode_view)).perform();
+    }
+
+
 
 
 }
